c03bb9f9af05afd3208b3d66351ca78be05dbeda
Saif <ofaruq281@gmail.com>
2025-10-14 00:34:53 +0600

    added toggling feature

--- scripts/home.js
+++ scripts/home.js
@@ -25,3 +25,68 @@ document.getElementById('add-money-btn').addEventListener('click', function (e) 
     document.getElementById('available-balance').innerText = newBalance
 
 })
+// function to toggle
+function handleToggle(id) {
+    const forms = document.getElementsByClassName("form")
+
+    for (const form of forms) {
+        form.style.display = "none"
+    }
+
+    document.getElementById(id).style.display = "block"
+}
+
+// function to toggle buttons
+function handleButtonToggle(id) {
+    const formBtns = document.getElementsByClassName("form-btn")
+
+    for (const btn of formBtns) {
+        btn.classList.remove("border-[#0874f2]", "bg-[#0874f20d]")
+        btn.classList.add("border-gray-300")
+
+    }
+
+    document.getElementById(id).classList.remove("border-gray-300")
+    document.getElementById(id).classList.add("border-[#0874f2]", "bg-[#0874f20d]")
+}
+
+
+// toggling feature
+
+document.getElementById("add-button").addEventListener("click", function () {
+    handleToggle("add-money-parent")
+
+    handleButtonToggle("add-button")
+
+
+})
+
+document.getElementById("cash-out-button").addEventListener("click", function () {
+    handleToggle("cash-out-parent")
+    handleButtonToggle("cash-out-button")
+})
+
+document.getElementById("transfer-money-button").addEventListener("click", function () {
+    handleToggle("transfer-money-parent")
+    handleButtonToggle("transfer-money-button")
+})
+
+
+document.getElementById("bonus-button").addEventListener("click", function () {
+    handleToggle("bonus-parent")
+    handleButtonToggle("bonus-button")
+
+})
+
+
+document.getElementById("pay-bill-button").addEventListener("click", function () {
+    handleToggle("pay-bill-parent")
+    handleButtonToggle("pay-bill-button")
+})
+
+
+document.getElementById("transactions-button").addEventListener("click", function () {
+    handleToggle("transactions-parent")
+    handleButtonToggle("transactions-button")
+
+})
